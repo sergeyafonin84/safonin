a3b7cec6c0030e50319dad660cafe59f8c2b7c6c
Sergey Afonin <afonin28@gmail.com>
2017-10-03 20:18:06 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,11 +1,19 @@
 package ru.job4j;
 
+/**
+*Calculate.
+*
+*@author Afonin Sergey (afonin1c@mail.ru)
+*@version 1
+@since 03.10.2017
+*/
 public class Calculate {
-
+	/**
+	*Main
+	*@param args -args
+	*/		
 	public static void main(String[] args) {
-
-	System.out.println("Hello World");
-
-}
+		System.out.println("Hello World");
+	}	
 
 }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,7 @@
+/**
+*Package for calculate task.
+*
+*@author Afonin Sergey (mailto:afonin1c@mail.ru)
+*@version 1
+*@since 03.10.2017
+*/
