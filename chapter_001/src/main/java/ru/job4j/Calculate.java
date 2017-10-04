21e1afe6b05325675b4510066db71c28a74c1228
Sergey Afonin <afonin28@gmail.com>
2017-10-04 19:46:48 +0300

    task 4123

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,28 +1,28 @@
 package ru.job4j;
 
 /**
-*Calculate.
-*
-*@author Afonin Sergey (afonin1c@mail.ru)
-*@version 1
-@since 03.10.2017
-*/
+ * Calculate.
+ *
+* @author Afonin Sergey (afonin1c@mail.ru)
+ * @version 01
+ * @since 04.10.2017
+ */
 public class Calculate {
 	/**
-	*Main
-	*@param args -args
-	*/		
+	* Main.
+	* @param args -args
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-	}
-	
-	/**
+    }
+
+    	/**
 	* Method echo.
 	* @param name Your name.
 	* @return Echo plus your name.
 	*/
 	public String echo(String name) {
 	    return "Echo, echo, echo : " + name;
-	}	
+	}
 
 }
--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -2,29 +2,32 @@
 package ru.job4j;
 
 import org.junit.Test;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+/*import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;*/
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
+*Test test.
 *
 *@author Afonin Sergey (mailto:afonin1c@mail.ru)
+*
 *@version 1
+*
 *@since 03.10.2017
 */
 
 public class CalculateTest {
 /**
 * Test echo.
-*/ @Test
+*/
+@Test
 public void whenTakeNameThenTreeEchoPlusName() {
     String input = "Afonin Sergey";
-    String expect = "Echo, echo, echo : Afonin Sergey"; 
+    String expect = "Echo, echo, echo : Afonin Sergey";
     Calculate calc = new Calculate();
     String result = calc.echo(input);
     assertThat(result, is(expect));
 }
- 
+
 }
