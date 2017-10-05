d639abceaee7204463bf81e4db13d151e6a9f9f8
Sergey Afonin <afonin28@gmail.com>
2017-10-05 14:59:55 +0300

    task 4417 idea learn

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -3,26 +3,26 @@ package ru.job4j;
 /**
  * Calculate.
  *
-* @author Afonin Sergey (afonin1c@mail.ru)
+ * @author Afonin Sergey (afonin1c@mail.ru)
  * @version 01
  * @since 04.10.2017
  */
 public class Calculate {
 	/**
-	* Main.
-	* @param args -args
-	*/
+	 * Main.
+	 * @param args -args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-    }
+	}
 
-    	/**
-	* Method echo.
-	* @param name Your name.
-	* @return Echo plus your name.
-	*/
+	/**
+	 * Method echo.
+	 * @param name Your name.
+	 * @return Echo plus your name.
+	 */
 	public String echo(String name) {
-	    return "Echo, echo, echo : " + name;
+		return "Echo, echo, echo : " + name;
 	}
 
 }
