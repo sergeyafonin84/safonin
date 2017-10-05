fbb18066cb1e0604cb133c270485ef12899929b2
Sergey Afonin <afonin28@gmail.com>
2017-10-05 15:33:28 +0300

    task 4417 idea learn3

--- chapter_001/src/main/java/ru/job4j/package-info.java
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -1,7 +1,7 @@
 /**
-*Package for calculate task.
-*
-*@author Afonin Sergey (mailto:afonin1c@mail.ru)
-*@version 1
-*@since 03.10.2017
-*/
+ *Package for calculate task.
+ *
+ *@author Afonin Sergey (mailto:afonin1c@mail.ru)
+ *@version 1
+ *@since 03.10.2017
+ */
--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -8,26 +8,26 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-*Test test.
-*
-*@author Afonin Sergey (mailto:afonin1c@mail.ru)
-*
-*@version 1
-*
-*@since 03.10.2017
-*/
+ *Test test.
+ *
+ *@author Afonin Sergey (mailto:afonin1c@mail.ru)
+ *
+ *@version 1
+ *
+ *@since 03.10.2017
+ */
 
 public class CalculateTest {
-/**
-* Test echo.
-*/
-@Test
-public void whenTakeNameThenTreeEchoPlusName() {
-    String input = "Afonin Sergey";
-    String expect = "Echo, echo, echo : Afonin Sergey";
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-}
+    /**
+     * Test echo.
+     */
+    @Test
+    public void whenTakeNameThenTreeEchoPlusName() {
+        String input = "Afonin Sergey";
+        String expect = "Echo, echo, echo : Afonin Sergey";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
 
 }
