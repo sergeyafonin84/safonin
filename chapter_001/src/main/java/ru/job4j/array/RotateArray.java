af222899ee0b0bd132e454665c09f894c6e13dd0
Sergey Afonin <afonin28@gmail.com>
2017-10-11 18:54:36 +0300

    "task 223"

--- chapter_001/src/main/java/ru/job4j/array/RotateArray.java
+++ chapter_001/src/main/java/ru/job4j/array/RotateArray.java
@@ -1,14 +1,25 @@
 package ru.job4j.array;
-
+/**
+ * Rotate array.
+ *
+ * @author Afonin Sergey (afonin1c@mail.ru)
+ * @version 01
+ * @since 11.10.2017
+ */
 public class RotateArray {
 
+    /**
+     * Method back Rotated array.
+     * @param array array to by rotated.
+     * @return rotated array.
+     */
     public int[][] rotate(int[][] array) {
 
         int[][] rotatedArray = new int[array.length][array.length];
 
-        for (int i = 0; i <= array.length-1; i++) {
+        for (int i = 0; i <= array.length - 1; i++) {
 
-            for (int k = 0; k <= array.length-1; k++) {
+            for (int k = 0; k <= array.length - 1; k++) {
 
                 rotatedArray[k][i] = array[array.length - 1 - i][k];
 
--- chapter_001/src/test/java/ru/job4j/array/RotateArrayTest.java
+++ chapter_001/src/test/java/ru/job4j/array/RotateArrayTest.java
@@ -4,24 +4,37 @@ import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-
+/**
+ *Test rotate array.
+ *
+ *@author Afonin Sergey (mailto:afonin1c@mail.ru)
+ *
+ *@version 1
+ *
+ *@since 11.10.2017
+ */
 public class RotateArrayTest {
+    /**
+     * Test rotating array.
+     */
     @Test
     public void whenRotateTwoRowTwoColArrayThenRotatedArray() {
 
-        int[][] testArray = {{1,2}, {3,4}};
-        int[][] expected = {{3,1}, {4,2}};
+        int[][] testArray = {{1, 2}, {3, 4}};
+        int[][] expected = {{3, 1}, {4, 2}};
 
         RotateArray rotateArray = new RotateArray();
         int[][]  result = rotateArray.rotate(testArray);
         assertThat(result, is(expected));
     }
-
+    /**
+     * Test rotating array.
+     */
     @Test
     public void whenRotateThreeRowThreeColArrayThenRotatedArray() {
 
-        int[][] testArray = {{1,2,3}, {4,5,6}, {7,8,9}};
-        int[][] expected = {{7,4,1}, {8,5,2}, {9,6,3}};
+        int[][] testArray = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] expected = {{7, 4, 1}, {8, 5, 2}, {9, 6, 3}};
 
         RotateArray rotateArray = new RotateArray();
         int[][]  result = rotateArray.rotate(testArray);
