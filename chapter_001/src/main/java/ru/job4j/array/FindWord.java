d62bc89102959da88a1d1709644ee17c8686b486
Sergey Afonin <afonin28@gmail.com>
2017-10-13 17:42:26 +0300

    "task 228"

--- chapter_001/src/main/java/ru/job4j/array/FindWord.java
+++ chapter_001/src/main/java/ru/job4j/array/FindWord.java
@@ -1,13 +1,25 @@
 package ru.job4j.array;
-
+/**
+ * checking that one word is in another word.
+ *
+ * @author Afonin Sergey (afonin1c@mail.ru)
+ * @version 01
+ * @since 13.10.2017
+ */
 public class FindWord {
 
-    public boolean FindSubWordInWord(String word, String subWord) {
+    /**
+     * Method FindSubWordInWord find sub word in word.
+     * @param word is a word in which need to find another word.
+     * @param subWord is a word what need to find in word.
+     * @return true then the word is find in another word.
+     */
+    public boolean findSubWordInWord(String word, String subWord) {
 
-        char[] arrayWord = TransformWordToArray(word);
-        char[] arraySubWord = TransformWordToArray(subWord);
+        char[] arrayWord = myTransformWordToArray(word);
+        char[] arraySubWord = myTransformWordToArray(subWord);
 
-        for (int i = 0; i < arrayWord.length - arraySubWord.length+1; i++) {
+        for (int i = 0; i < arrayWord.length - arraySubWord.length + 1; i++) {
             for (int j = 0; j < arraySubWord.length; j++) {
                 if (arraySubWord[j] != arrayWord[i + j]) {
                     break;
@@ -20,7 +32,13 @@ public class FindWord {
         return  false;
     }
 
-    private char[] TransformWordToArray(String word) {
+    /**
+     * Method TransformWordToArray transforms String into array.
+     * @param word is a string type variable.
+     * @return array from string.
+     */
+    private char[] myTransformWordToArray(String word) {
+
         int lengthWord = word.length();
         char[] arrayWord = new char[lengthWord];
         for (int i = 0; i < lengthWord; i++) {
--- chapter_001/src/test/java/ru/job4j/array/FindWordTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindWordTest.java
@@ -1,13 +1,20 @@
 package ru.job4j.array;
 
-import com.sun.xml.internal.ws.api.ha.StickyFeature;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+/**
+ * Test finding one word in another.
+ * @author Afonin Sergey (mailto:afonin1c@mail.ru).
+ * @version 1
+ * @since 13.10.2017
+ */
 public class FindWordTest {
-
+    /**
+     * Test finding one word in another
+     */
     @Test
     public void whenFindsInAnoterWord() {
         //напишите здесь тест, проверяющий сортировку массива из 10 элементов методом пузырька, например {1, 5, 4, 2, 3, 1, 7, 8, 0, 5}.
