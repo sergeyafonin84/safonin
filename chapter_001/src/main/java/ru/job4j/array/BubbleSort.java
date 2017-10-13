386f96885d5a8c29498e771e8262d57061032c88
Sergey Afonin <afonin28@gmail.com>
2017-10-13 17:22:27 +0300

    "mvn validate"

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -14,12 +14,12 @@ public class BubbleSort {
      * @return sorted array.
      */
     public int[] sort(int[] array) {
-        for (int i = array.length - 1; i>0; i--) {
-            for (int j = 0; j<i; j++) {
-                if (array[j] > array[j+1]) {
+        for (int i = array.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
                     int tmp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = tmp;
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
                 }
             }
         }
--- chapter_001/src/main/java/ru/job4j/array/CombineSortedArrays.java
+++ chapter_001/src/main/java/ru/job4j/array/CombineSortedArrays.java
@@ -13,33 +13,30 @@ public class CombineSortedArrays {
      * @param array2 array for combing.
      * @return combined ordered array.
      */
-    public int[] combineSortedArraysIntoSortedArray(int[] array1,int[] array2) {
+    public int[] combineSortedArraysIntoSortedArray(int[] array1, int[] array2) {
 
         int maxIndArr1 = array1.length - 1;
         int maxIndArr2 = array2.length - 1;
         int currentIndArr1 = 0;
         int currentIndArr2 = 0;
         int maxInCombinedArr = array1.length + array2.length - 1;
-        int[] combinedArray = new int[maxInCombinedArr+1];
+        int[] combinedArray = new int[maxInCombinedArr + 1];
 
         for (int i = 0; i <= maxInCombinedArr; i++) {
 
-            if(maxIndArr1 < currentIndArr1) {
+            if (maxIndArr1 < currentIndArr1) {
                 combinedArray[i] = array2[currentIndArr2];
                 currentIndArr2 = currentIndArr2 + 1;
                 continue;
-            }
-            else if (maxIndArr2 < currentIndArr2) {
+            } else if (maxIndArr2 < currentIndArr2) {
                 combinedArray[i] = array1[currentIndArr1];
                 currentIndArr1 = currentIndArr1 + 1;
                 continue;
-            }
-            else if (array1[currentIndArr1] < array2[currentIndArr2]) {
+            } else if (array1[currentIndArr1] < array2[currentIndArr2]) {
                 combinedArray[i] = array1[currentIndArr1];
                 currentIndArr1 = currentIndArr1 + 1;
                 continue;
-            }
-            else {
+            } else {
                 combinedArray[i] = array2[currentIndArr2];
                 currentIndArr2 = currentIndArr2 + 1;
             }
