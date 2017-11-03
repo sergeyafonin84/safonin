51e51a89b5806c5f08068f069259c11aa8774ae9
Sergey Afonin <afonin28@gmail.com>
2017-11-03 11:57:43 +0300

    task 10035 (3. Конвертация двумерного массива в ArrayList и наоборот [#10035])

---
+++ chapter_003/src/main/java/ru.job4j/generic/ConvertList.java
@@ -0,0 +1,77 @@
+package ru.job4j.generic;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConvertList {
+
+    public List<Integer> toList(int[][] array) {
+
+        List<Integer> resultList = new ArrayList<Integer>();
+
+        for (int[] currentArray : array) {
+            for (int currentElement : currentArray) {
+                resultList.add(currentElement);
+            }
+        }
+        return  resultList;
+    }
+
+    public int[][] toArray(List<Integer> list, int rows) {
+
+        int[][] resultArray = new int[rows][];
+
+        int quontityOfElements = list.size();
+        int remainderOfTheDivision = quontityOfElements % rows;
+        int realNumberOfRows = quontityOfElements - remainderOfTheDivision;
+
+        if (quontityOfElements >= rows) {
+
+            int finalNumberInColumn = (quontityOfElements - remainderOfTheDivision) / (rows - 1);
+
+            int totalNumberOfElements = finalNumberInColumn * rows;
+
+            resultArray = new int[rows][finalNumberInColumn];
+
+            int currenNumberOfRow = 0;
+            int currentNumberOfColumn = 0;
+            int currentNumberOfElement = 1;
+
+            for (int element : list) {
+
+                resultArray[currenNumberOfRow][currentNumberOfColumn] = element;
+                if (currentNumberOfColumn < finalNumberInColumn - 1) {
+                    currentNumberOfColumn++;
+                } else {
+                    currentNumberOfColumn = 0;
+                    currenNumberOfRow++;
+                }
+                currentNumberOfElement++;
+            }
+
+            while (currentNumberOfElement < totalNumberOfElements) {
+                resultArray[currenNumberOfRow][currentNumberOfColumn] = 0;
+                currentNumberOfElement++;
+                currentNumberOfColumn++;
+            }
+
+        } else {
+            System.out.println("Wrong number of rows!");
+        }
+            return resultArray;
+    }
+
+//    public List<Integer> convert(List<int[]> list) {
+//
+//        List<Integer> returnList = new ArrayList<>();
+//
+//        for (int[] currentArray : list) {
+//
+//            for (int currentElement : currentArray) {
+//
+//                returnList.add(currentElement);
+//            }
+//        }
+//        return returnList;
+//    }
+
+}
---
+++ chapter_003/src/test/java/ru/job4j/genereic/ConvertListTest.java
@@ -0,0 +1,82 @@
+package ru.job4j.genereic;
+
+import org.junit.Test;
+import ru.job4j.generic.ConvertList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+
+public class ConvertListTest {
+
+    @Test
+    public void whenConvertOtArrayThenGetArray() {
+
+        List<Integer> list = new ArrayList<Integer>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+
+        ConvertList convertList = new ConvertList();
+        int[][] result = convertList.toArray(list, 3);
+
+        int[][] expected = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 0, 0}};
+
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenConvertToListThenGetList() {
+
+        int[][] array = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 0, 0}};
+
+        ConvertList convertList = new ConvertList();
+
+        List<Integer> result = convertList.toList(array);
+
+        List<Integer> expected = new ArrayList<Integer>();
+
+        expected.add(1);
+        expected.add(2);
+        expected.add(3);
+        expected.add(4);
+        expected.add(5);
+        expected.add(6);
+        expected.add(7);
+        expected.add(0);
+        expected.add(0);
+
+        assertThat(result, is(expected));
+    }
+
+//    @Test
+//    public void whenConvertListOfArraysThenGetList() {
+//
+//        List<int[]> list = new ArrayList<int[]>();
+//
+//        list.add(new int[]{1, 2});
+//        list.add(new int[]{3, 4, 5, 6});
+//
+//        List<Integer> result = new ConvertList().convert(list);
+//
+//        List<Integer> expected = new ArrayList<Integer>();
+//
+//        expected.add(1);
+//        expected.add(2);
+//        expected.add(3);
+//        expected.add(4);
+//        expected.add(5);
+//        expected.add(6);
+//
+//        assertThat(result, is(expected));
+//    }
+}
