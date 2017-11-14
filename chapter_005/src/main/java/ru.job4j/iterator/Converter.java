7e04478a665a40e5698bb5048cb6296a8513c73d
Sergey Afonin <afonin28@gmail.com>
2017-11-14 22:15:52 +0300

    task 152 WORK ON BUGS 5.1.4. Создать convert(Iterator<Iterator>) [#152][#30870]

--- chapter_005/src/main/java/ru.job4j/iterator/Converter.java
+++ chapter_005/src/main/java/ru.job4j/iterator/Converter.java
@@ -1,6 +1,5 @@
 package ru.job4j.iterator;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -8,54 +7,51 @@ public class Converter {
 
     Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
 
-
-
         return new Iterator<Integer>() {
 
+            private Iterator<Integer> iterator;
 
-            private final ArrayList<Integer> values = getValuesFromIteratorIterator();
-            private int index = 0;
+            private boolean hasNextIteratorIterator() {
 
-
-
-            ArrayList<Integer> getValuesFromIteratorIterator() {
-
-                ArrayList<Integer> valuesFromIteratorIterator = new ArrayList<Integer>();
-
-                while (it.hasNext()) {
-
-                    Iterator<Integer> currIterator = it.next();
-
-                    while (currIterator.hasNext()) {
-
-                        valuesFromIteratorIterator.add(currIterator.next());
-
-                    }
-                }
-                return valuesFromIteratorIterator;
+                return it.hasNext();
             }
 
+            private boolean hasNextIterator() {
 
+                return iterator.hasNext();
+            }
 
             @Override
             public boolean hasNext() {
 
-                return values.size() > index;
-
+                return hasNextIteratorIterator() || hasNextIterator();
             }
 
             @Override
             public Integer next() {
 
-                if (!this.hasNext()) {
+                if (!hasNext()) {
+
                     throw new NoSuchElementException();
+
                 } else {
-                    return values.get(index++);
+
+                    if (iterator == null || !hasNextIterator()) {
+
+                        iterator = it.next();
+
+                        return iterator.next();
+
+                    } else {
+
+                        return iterator.next();
+                    }
                 }
+
             }
+
         };
 
-
-
     }
+
 }
