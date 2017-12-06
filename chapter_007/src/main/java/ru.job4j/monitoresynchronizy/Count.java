33e7386a99c7c5d22ab2c3609e6909f4a9e5e16d
Sergey Afonin <afonin28@gmail.com>
2017-12-06 23:14:31 +0300

    task 1102 (1. класс Count и метод int incremant() [#1102]) //1. Сделать класс многопоточный счетчик Count#int incremant(). //2. Подключить библиотеку jcip-annotations //3. В заголовке класса указать аннотацию @ThreadSafe //4. Для поля состояния использовать аннотацию @GuardedBy //5. В аннотации GuardedBy - указать объект монитора.

---
+++ chapter_007/src/main/java/ru.job4j/monitoresynchronizy/Count.java
@@ -0,0 +1,55 @@
+package ru.job4j.monitoresynchronizy;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+
+//1. Сделать класс многопоточный счетчик Count#int incremant().
+//2. Подключить библиотеку jcip-annotations
+//3. В заголовке класса указать аннотацию @ThreadSafe
+//4. Для поля состояния использовать аннотацию @GuardedBy
+//5. В аннотации GuardedBy - указать объект монитора.
+@ThreadSafe
+public class Count {
+
+    public static final class Counter {
+        // https://samxiangyu.wordpress.com/2015/02/20/java-concurrency-guardedby/
+        @GuardedBy("this")
+        long count = 0;
+
+        public void add(long value) {
+
+            this.count += value;
+        }
+    }
+
+
+    public static final class CounterThread extends Thread {
+        protected final Counter counter;
+
+        public CounterThread(final Counter counter) {
+            this.counter = counter;
+        }
+
+        @Override
+        public void run() {
+            counter.add(1);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+        Thread threadA = new CounterThread(counter);
+        Thread threadB = new CounterThread(counter);
+
+
+        threadA.start();
+        threadB.start();
+        threadA.join();
+        threadB.join();
+        System.out.println(counter.count);
+    }
+
+}
+
+
