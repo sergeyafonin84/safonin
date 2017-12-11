1680b7792d7a7238d97689eaf81e87ee186c896a
Sergey Afonin <afonin28@gmail.com>
2017-12-11 21:10:25 +0300

    examples technical commit: Deadlock and SynchronizedThread

---
+++ chapter_007/src/main/java/ru.job4j/monitoresynchronizy/examples/SynchronizedThread.java
@@ -0,0 +1,38 @@
+package ru.job4j.monitoresynchronizy.examples;
+
+class CommonObject {
+    int counter = 0;
+}
+
+class CounterThread implements Runnable {
+    CommonObject res;
+    CounterThread(CommonObject res) {
+        this.res = res;
+    }
+
+    @Override
+    public void run() {
+        synchronized (res) {
+            res.counter = 1;
+            for (int i = 1; i < 5; i++) {
+                System.out.printf("'%s' - %d\n",
+                        Thread.currentThread().getName(), res.counter);
+                res.counter++;
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) { }
+            }
+        }
+    }
+}
+public class SynchronizedThread {
+    public static void main(String[] args) {
+        CommonObject commonObject = new CommonObject();
+        for (int i = 1; i < 6; i++) {
+            Thread t = new Thread(new CounterThread(commonObject));
+            t.setName("Поток " + i);
+            t.start();
+        }
+    }
+}
+
